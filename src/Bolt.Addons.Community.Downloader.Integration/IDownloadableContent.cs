00c577da10e09086b613a1f2a7cf228a493b2b5a
RealityStop <qubiquityCodes@gmail.com>
2018-04-21 01:57:20 -0400

    Beginning work on downloader

---
+++ src/Bolt.Addons.Community.Downloader.Integration/IDownloadableContent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bolt.Addons.Community.Downloader.Integration
+{
+    interface IDownloadableContent
+    {
+        List<Uri> DownloadURIs { get; set; }
+
+        Task<bool> Download();
+    }
+}
